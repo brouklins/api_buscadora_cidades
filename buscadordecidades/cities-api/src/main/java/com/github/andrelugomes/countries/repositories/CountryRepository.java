a7335f5c153757544444ef5ac172dc1fc6ca00ef
Fernando Oliveira <80929076+brouklins@users.noreply.github.com>
2021-08-11 15:45:49 -0300

    Delete buscadordecidades/cities-api/src/main/java/com/github/andrelugomes directory

--- buscadordecidades/cities-api/src/main/java/com/github/andrelugomes/countries/repositories/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.andrelugomes.countries.repositories;
-
-import com.github.andrelugomes.countries.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- buscadordecidades/cities-api/src/main/java/com/github/andrelugomes/staties/repositories/StateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.andrelugomes.staties.repositories;
-
-import com.github.andrelugomes.staties.entities.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
